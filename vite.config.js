31c375615c9fb9678bf2e47ef5b0a52deac83403
William-Bowman <will.j.bowman27@gmail.com>
2025-06-26 14:17:32 +1000

    Moved files out of /public after reading tf it does, and added entrypoints to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
     plugins: [
         tailwindcss(),
     ],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                blog: resolve(__dirname, 'blog.html'),
+            },
+        },
+    },
 })
